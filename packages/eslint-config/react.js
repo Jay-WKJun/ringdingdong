1e147254a6db35672bb30c621235fe15b33c810a
KyungJun Woo <natcho9010@gmail.com>
2024-02-13 20:05:58 +0900

    feat: change eslint rule

--- packages/eslint-config/react.js
+++ packages/eslint-config/react.js
@@ -32,5 +32,6 @@ module.exports = {
 
     'jsx-a11y/no-static-element-interactions': 'off',
     'jsx-a11y/click-events-have-key-events': 'off',
+    'jsx-a11y/mouse-events-have-key-events': 'off',
   },
 };
