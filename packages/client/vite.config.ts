17fed3ce1a4c7766f21c69a4b9be207cacf2ea6d
KyungJun Woo <natcho9010@gmail.com>
2024-02-16 19:28:34 +0900

    chore: vite ts path

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,7 +1,11 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({ jsxImportSource: "@emotion/react", babel: { plugins: ["@emotion/babel-plugin"] } })],
+  plugins: [
+    react({ jsxImportSource: "@emotion/react", babel: { plugins: ["@emotion/babel-plugin"] } }),
+    tsconfigPaths(),
+  ],
 });
