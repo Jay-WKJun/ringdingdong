f2b39265cc3a2eddc2e5d96f67efa11c77b6ae48
KyungJun Woo <natcho9010@gmail.com>
2024-02-25 22:31:00 +0900

    feat: add healthCheck api function

--- packages/client/src/apis/userApis.ts
+++ packages/client/src/apis/userApis.ts
@@ -1,4 +1,12 @@
-// 헬스체크
+export function getHealthCheck(serverUrl: string) {
+  try {
+    fetch(`${serverUrl}/health`).then(() => {
+      console.log("Server is running");
+    });
+  } catch {
+    console.error("Server is not running");
+  }
+}
 
 // 토큰 검증
 
