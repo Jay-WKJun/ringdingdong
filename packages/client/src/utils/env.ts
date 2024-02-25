012a003595ebebb5d0cee5cb78fa6157fa91c622
KyungJun Woo <natcho9010@gmail.com>
2024-02-25 23:11:37 +0900

    fix: env setting in client

--- packages/client/src/utils/env.ts
+++ packages/client/src/utils/env.ts
@@ -1,6 +1,3 @@
 const DEV_SERVER_URL = "http://localhost:5173";
 
-export const SERVER_URL =
-  process.env.ENV === "dev"
-    ? DEV_SERVER_URL
-    : import.meta.env.VITE_SERVER_URL || DEV_SERVER_URL;
+export const SERVER_URL = DEV_SERVER_URL;
