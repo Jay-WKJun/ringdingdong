90638485d77c683ed8ccd3e9e561232e392869bf
KyungJun Woo <natcho9010@gmail.com>
2024-02-25 22:45:59 +0900

    chore: add env in client

--- packages/client/src/utils/env.ts
+++ packages/client/src/utils/env.ts
@@ -1,1 +1,6 @@
-export const SERVER_URL = import.meta.env.VITE_SERVER_URL || "";
+const DEV_SERVER_URL = "http://localhost:5173";
+
+export const SERVER_URL =
+  process.env.ENV === "dev"
+    ? DEV_SERVER_URL
+    : import.meta.env.VITE_SERVER_URL || DEV_SERVER_URL;
